afed7ffd40b6218947431782096d09e218f6d055
Mrtut <tutmurattut@gmail.com>
2026-02-07 17:39:49 +0300

    Remove dotenv
    It seems dotenv cannot use in frontend, since it it using node.js and I forget that

--- client/app/auth_client.ts
+++ client/app/auth_client.ts
@@ -1,6 +1,4 @@
 import { createAuthClient } from "better-auth/react"
-import dotenv from "dotenv";
-dotenv.config();
 
 export const { signIn, signUp, useSession, signOut } = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
