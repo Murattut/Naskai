788617812ef131fa972dd8edb362bc078ed8debd
Mrtut <tutmurattut@gmail.com>
2026-02-07 17:33:18 +0300

    Fix auth url address

--- client/app/auth_client.ts
+++ client/app/auth_client.ts
@@ -1,5 +1,8 @@
 import { createAuthClient } from "better-auth/react"
+import dotenv from "dotenv";
+dotenv.config();
+
 export const { signIn, signUp, useSession, signOut } = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: "http://localhost:8000"
+    baseURL: process.env.BASE_URL || "http://localhost:8000"
 })
